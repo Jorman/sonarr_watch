b5c7e3408baea62c14c6e543e57d50e99953a8df
James Heimberger <james.heimberger@gmail.com>
2020-01-17 20:19:51 -0500

    Added slightly better logging suppport

--- handler.py
+++ handler.py
@@ -1,9 +1,15 @@
 import os
+import sys
 
 import rarfile
 from watchdog.events import FileSystemEventHandler
 
+def log(msg):
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+
 class Handler(FileSystemEventHandler):
+    
 
     @staticmethod
     def on_any_event(event):
@@ -23,7 +29,7 @@ class Handler(FileSystemEventHandler):
                     if file.endswith('.rar'):
                         rar_file = file
                     if file.startswith('.syncthing'):
-                        print('Sync file located.\n')
+                        log(f'Sync file located in {curr_dir}')
                         sync_file = True
                         rar_file = None
                         break
@@ -32,12 +38,12 @@ class Handler(FileSystemEventHandler):
                     if rar_file is not None:
                         rar = rarfile.RarFile(f'/{curr_dir}/{rar_file}')
                         try:
-                            print(f'Extracting {rar_file}\n')
+                            log(f'Extracting {rar_file}\n')
                             rar.extractall(path=curr_dir)
                         except:
                             return None
                         rar_list = rar.namelist()
-                        print(f'Cleaning up {curr_dir}\n')
+                        log(f'Cleaning up {curr_dir}\n')
                         for f in os.listdir(curr_dir):
                             if f not in rar_list:
                                 os.remove(f'{curr_dir}/{f}')
