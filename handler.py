40eb2b9733d86a887a3d7da5f94a53447cc0a88f
James Heimberger <james.heimberger@gmail.com>
2020-01-17 19:25:38 -0500

    Removed some unneeded prints

--- handler.py
+++ handler.py
@@ -10,14 +10,14 @@ class Handler(FileSystemEventHandler):
         sync_file = False
         rar_file = None
 
-        if event.event_type == 'deleted':
-            print('delete')
-            return None
+        #if event.event_type == 'deleted':
+        #    print('delete')
+        #    return None
         elif event.is_directory is True:
             #print(f"\n\n!!!!!! - {event.event_type} - !!!!!!\n\n")
             if event.event_type == 'created' or event.event_type == 'modified':
                 curr_dir = event.src_path
-                print(f'Received {event.event_type} event - {curr_dir}')
+                #print(f'Received {event.event_type} event - {curr_dir}')
                 
                 for file in os.listdir(curr_dir):
                     print(file)
@@ -25,7 +25,7 @@ class Handler(FileSystemEventHandler):
                         rar_file = file
                         print(rar_file)
                     if file.startswith('.syncthing'):
-                        print('Sync file located.')
+                        print('Sync file located.\n')
                         sync_file = True
                         rar_file = None
                         break
@@ -34,6 +34,7 @@ class Handler(FileSystemEventHandler):
                     if rar_file is not None:
                         rar = rarfile.RarFile(f'/{curr_dir}/{rar_file}')
                         try:
+                            print(f'Extracting {rar_file}\n')
                             rar.extractall(path=curr_dir)
                         except:
                             return None
@@ -42,7 +43,7 @@ class Handler(FileSystemEventHandler):
                             if f not in rar_list:
                                 os.remove(f'{curr_dir}/{f}')
                         rar_file = None
-                        print('!!\n\n')
+                        #print('!!\n\n')
                     else:
                         print('No RAR file located.')
                     
